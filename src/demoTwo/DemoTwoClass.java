5338c255539a34056a26458b2ffd991171c8ac6e
Owner <Owner@DESKTOP-GMJN2FU.myfiosgateway.com>
2021-11-24 19:58:19 -0500

    ADDED ADD() METHOD

--- src/demoTwo/DemoTwoClass.java
+++ src/demoTwo/DemoTwoClass.java
@@ -5,5 +5,12 @@ public class DemoTwoClass {
 	public static void main(String[] args) {
 		System.out.println("This is demo two...");
 	}
+		
+	
+	public static void add(int i, int x) {
+		System.out.println(i+x);
+	
+	}
+	}
 
-}
+
